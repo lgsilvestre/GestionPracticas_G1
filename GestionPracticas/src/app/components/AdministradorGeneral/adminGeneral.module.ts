40da53bdaeffa6ccc9134166133a8f18a927590b
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2021-04-21 19:04:51 -0400

    interfaz parte 1

--- GestionPracticas/src/app/components/AdministradorGeneral/adminGeneral.module.ts
+++ GestionPracticas/src/app/components/AdministradorGeneral/adminGeneral.module.ts
@@ -1,14 +1,16 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MenuAdminGeneralComponent } from "./menu-admin-general/menu-admin-general.component";
-import { MaterialModule } from "src/app/material/material.module";
-import { ReactiveFormsModule } from "@angular/forms";
-import { GraficosModule } from "../Graficos/graficos.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuAdminGeneralComponent } from './menu-admin-general/menu-admin-general.component';
+import { MaterialModule } from 'src/app/material/material.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GraficosModule } from '../Graficos/graficos.module';
+import { PlantillaGeneralComponent } from './plantilla-general/plantilla-general.component';
 
 @NgModule({
   declarations: [
     // Aca van los modulos que se crean
     MenuAdminGeneralComponent,
+    PlantillaGeneralComponent,
 
   ],
   imports: [
