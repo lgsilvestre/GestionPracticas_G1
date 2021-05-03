5eac5f6e207244cb1d441ad8083653d8a3442e88
Diego <dbarahona14@alumnos.utalca.cl>
2021-05-03 16:39:31 -0400

    Se habilita filtro buscar en general
    Se puede buscar en tabla visualizar prácticas por el nombre del estudiante.

--- GestionPracticas/src/app/components/EncargadoCarrera/visualizar-practica/visualizar-practica.component.ts
+++ GestionPracticas/src/app/components/EncargadoCarrera/visualizar-practica/visualizar-practica.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { MatTableDataSource } from "@angular/material/table";
 
 export interface ITablaVisualizarPractica {
     rut: string;
@@ -7,6 +8,19 @@ export interface ITablaVisualizarPractica {
     empresa: string;
     ver: string;
 }
+
+const ELEMENT_DATA: ITablaVisualizarPractica[] = [
+    {position: 1, rut: '1111111', nombre: 'Juan Gonzalez R.', empresa: 'Empresa 1', ver: ''},
+    {position: 2, rut: '2222222', nombre: 'Maria Ramirez G.', empresa: 'Empresa 2', ver: ''},
+    {position: 3, rut: '3333333', nombre: 'Roberto Donoso J.', empresa: 'Empresa 3', ver: ''},
+    {position: 4, rut: '4444444', nombre: 'Martina Gonzalez R.', empresa: 'Empresa 4', ver: ''},
+    {position: 5, rut: '5555555', nombre: 'Clodotea Marambio E.', empresa: 'Empresa 5', ver: ''},
+    {position: 6, rut: '6666666', nombre: 'Ricardo Paredes G.', empresa: 'Empresa 6', ver: ''},
+    {position: 7, rut: '7777777', nombre: 'Lucia Marszz T.', empresa: 'Empresa 7', ver: ''},
+    {position: 8, rut: '8888888', nombre: 'Miguel Sanhueza K.', empresa: 'Empresa 8', ver: ''},
+    {position: 9, rut: '9999999', nombre: 'Ignacia Marambio B.', empresa: 'Empresa 9', ver: ''},
+    {position: 10, rut: '0000000', nombre: 'Francisco Duque Q.', empresa: 'Empresa 10', ver: ''},
+];
 
 
 @Component({
@@ -22,19 +36,29 @@ export class VisualizarComponent implements OnInit
     filtroEmpresaSeleccionado : boolean = false;
 
     displayedColumns: string[] = ['position', 'rut', 'nombre', 'empresa', 'ver'];
-    dataSource: ITablaVisualizarPractica[] = 
-    [
-        {position: 1, rut: '1111111', nombre: 'Juan Gonzalez R.', empresa: 'Empresa 1', ver: ''},
-        {position: 2, rut: '2222222', nombre: 'Maria Ramirez G.', empresa: 'Empresa 2', ver: ''},
-        {position: 3, rut: '3333333', nombre: 'Roberto Donoso J.', empresa: 'Empresa 3', ver: ''},
-        {position: 4, rut: '4444444', nombre: 'Martina Gonzalez R.', empresa: 'Empresa 4', ver: ''},
-        {position: 5, rut: '5555555', nombre: 'Clodotea Marambio E.', empresa: 'Empresa 5', ver: ''},
-        {position: 6, rut: '6666666', nombre: 'Ricardo Paredes G.', empresa: 'Empresa 6', ver: ''},
-        {position: 7, rut: '7777777', nombre: 'Lucia Marszz T.', empresa: 'Empresa 7', ver: ''},
-        {position: 8, rut: '8888888', nombre: 'Miguel Sanhueza K.', empresa: 'Empresa 8', ver: ''},
-        {position: 9, rut: '9999999', nombre: 'Ignacia Marambio B.', empresa: 'Empresa 9', ver: ''},
-        {position: 10, rut: '0000000', nombre: 'Francisco Duque Q.', empresa: 'Empresa 10', ver: ''},
-    ];
+
+    
+
+    dataSource = new MatTableDataSource(ELEMENT_DATA);
+
+    // dataSource: ITablaVisualizarPractica[] = 
+    // [
+    //     {position: 1, rut: '1111111', nombre: 'Juan Gonzalez R.', empresa: 'Empresa 1', ver: ''},
+    //     {position: 2, rut: '2222222', nombre: 'Maria Ramirez G.', empresa: 'Empresa 2', ver: ''},
+    //     {position: 3, rut: '3333333', nombre: 'Roberto Donoso J.', empresa: 'Empresa 3', ver: ''},
+    //     {position: 4, rut: '4444444', nombre: 'Martina Gonzalez R.', empresa: 'Empresa 4', ver: ''},
+    //     {position: 5, rut: '5555555', nombre: 'Clodotea Marambio E.', empresa: 'Empresa 5', ver: ''},
+    //     {position: 6, rut: '6666666', nombre: 'Ricardo Paredes G.', empresa: 'Empresa 6', ver: ''},
+    //     {position: 7, rut: '7777777', nombre: 'Lucia Marszz T.', empresa: 'Empresa 7', ver: ''},
+    //     {position: 8, rut: '8888888', nombre: 'Miguel Sanhueza K.', empresa: 'Empresa 8', ver: ''},
+    //     {position: 9, rut: '9999999', nombre: 'Ignacia Marambio B.', empresa: 'Empresa 9', ver: ''},
+    //     {position: 10, rut: '0000000', nombre: 'Francisco Duque Q.', empresa: 'Empresa 10', ver: ''},
+    // ];
+
+    applyFilter(event: Event) {
+        const filterValue = (event.target as HTMLInputElement).value;
+        this.dataSource.filter = filterValue.trim().toLowerCase();
+    }
 
     ngOnInit(): void 
     {
