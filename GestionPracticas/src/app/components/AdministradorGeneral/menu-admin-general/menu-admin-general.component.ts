d925c1fc91853c4beb5f0e029f06fdf8b9b25151
Vicente Calderon <thunderwearsvicho37@protonmail.com>
2021-05-03 11:12:21 -0400

    Style globalization

--- GestionPracticas/src/app/components/AdministradorGeneral/menu-admin-general/menu-admin-general.component.ts
+++ GestionPracticas/src/app/components/AdministradorGeneral/menu-admin-general/menu-admin-general.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-menu-admin-general',
-  templateUrl: './menu-admin-general.component.html',
-  styleUrls: ['./menu-admin-general.component.css']
+	selector: 'app-menu-admin-general',
+	templateUrl: './menu-admin-general.component.html',
+	styleUrls: ['./menu-admin-general.component.css', '../../../app.component.css']
 })
 export class MenuAdminGeneralComponent implements OnInit {
 
-  constructor() {
-  }
+	constructor() {
+	}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+	}
 
 }
