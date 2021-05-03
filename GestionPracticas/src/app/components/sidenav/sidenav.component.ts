5569f725f9996e81efcca85a8acd666e0e3da9c0
Vicente Calderon <thunderwearsvicho37@protonmail.com>
2021-05-03 12:00:39 -0400

    Login fix

--- GestionPracticas/src/app/components/sidenav/sidenav.component.ts
+++ GestionPracticas/src/app/components/sidenav/sidenav.component.ts
@@ -1,59 +1,58 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
-  selector: 'app-sidenav',
-  templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+	selector: 'app-sidenav',
+	templateUrl: './sidenav.component.html',
+	styleUrls: ['./sidenav.component.css', '../../app.component.css']
 })
-export class SidenavComponent implements OnInit
-{
-  user:any = JSON.parse(localStorage.getItem('user') || '{}');
-  soyEstudiante:boolean = false;
-  soyAdminGeneral:boolean = false;
-  soyEncargadoDeCarrera : boolean = false;
-  constructor(public auth: AngularFireAuth){
-  }
+export class SidenavComponent implements OnInit {
+	user: any = JSON.parse(localStorage.getItem('user') || '{}');
+	soyEstudiante: boolean = false;
+	soyAdminGeneral: boolean = false;
+	soyEncargadoDeCarrera: boolean = false;
+	constructor(public auth: AngularFireAuth) {
+	}
 
-  ngOnInit(): void {
-    console.log("ngOnInit");
-    if(this.user.rol == "estudiante"){
-      this.soyEstudiante = true;
-      this.soyAdminGeneral = this.soyEncargadoDeCarrera = false;
-    }
-    if(this.user.rol == "administradorGeneral"){
-      this.soyAdminGeneral = true;
-      this.soyEstudiante = this.soyEncargadoDeCarrera = false;
-    }
-    if (this.user.rol == "encargadoCarrera") {
-      this.soyEncargadoDeCarrera = true;
-      this.soyEstudiante = this.soyAdminGeneral = false;
-    }
-    this.user = JSON.parse(localStorage.getItem('user') || '{}');
-  }
+	ngOnInit(): void {
+		console.log("ngOnInit");
+		if (this.user.rol == "estudiante") {
+			this.soyEstudiante = true;
+			this.soyAdminGeneral = this.soyEncargadoDeCarrera = false;
+		}
+		if (this.user.rol == "administradorGeneral") {
+			this.soyAdminGeneral = true;
+			this.soyEstudiante = this.soyEncargadoDeCarrera = false;
+		}
+		if (this.user.rol == "encargadoCarrera") {
+			this.soyEncargadoDeCarrera = true;
+			this.soyEstudiante = this.soyAdminGeneral = false;
+		}
+		this.user = JSON.parse(localStorage.getItem('user') || '{}');
+	}
 
-  cambiarAdmin(){
-    this.soyAdminGeneral = true;
-    this.soyEstudiante = false;
-    this.soyEncargadoDeCarrera = false;
-  }
+	cambiarAdmin() {
+		this.soyAdminGeneral = true;
+		this.soyEstudiante = false;
+		this.soyEncargadoDeCarrera = false;
+	}
 
-  cambiarEstudiante(){
-    this.soyEstudiante = true;
-    this.soyAdminGeneral = false;
-    this.soyEncargadoDeCarrera = false;
-  }
+	cambiarEstudiante() {
+		this.soyEstudiante = true;
+		this.soyAdminGeneral = false;
+		this.soyEncargadoDeCarrera = false;
+	}
 
-  cambiarEncargadoCarrera(){
-    this.soyEncargadoDeCarrera = true;
-    this.soyAdminGeneral = false;
-    this.soyEstudiante = false;
-  }
+	cambiarEncargadoCarrera() {
+		this.soyEncargadoDeCarrera = true;
+		this.soyAdminGeneral = false;
+		this.soyEstudiante = false;
+	}
 
-  deslogear(){
-    this.user = '';
-    localStorage.setItem('user',JSON.stringify(this.user));
-    this.auth.signOut();
-  }
+	deslogear() {
+		this.user = '';
+		localStorage.setItem('user', JSON.stringify(this.user));
+		this.auth.signOut();
+	}
 
 }
