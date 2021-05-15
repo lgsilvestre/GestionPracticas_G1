95f76e863ebe836a9658c156046f369da4e9f1e6
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2021-05-15 00:54:22 -0400

    pruebas con formulario de crar usuario

--- GestionPracticas/src/app/components/SuperAdministrador/crear-cuentas-sa/crear-cuentas-sa.component.ts
+++ GestionPracticas/src/app/components/SuperAdministrador/crear-cuentas-sa/crear-cuentas-sa.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+/* tslint:disable:no-inferrable-types */
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {EncargadoCarrera} from '../../../model/encargadoCarrera.model';
+import {MatDialog} from '@angular/material/dialog';
+import {DialogElementsExampleDialogComponent} from '../dialog/dialog-elements-example-dialog/dialog-elements-example-dialog.component';
 
 @Component({
   selector: 'app-crear-cuentas-sa',
@@ -6,10 +11,60 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-cuentas-sa.component.css']
 })
 export class CrearCuentasSaComponent implements OnInit {
+  nuevaCuenta: FormGroup;
+  carreras: string[] = ['Ing. civil en computación', 'Ing. civil industrial', 'Ing. civil en obras civiles', 'Ing. civil mecatrónica'];
+  roles: string[] = ['Encargado de Carrera', 'Estudiante', 'Administrador General'];
+  carreraActual: string = 'None' ;
+  rolActual: string = 'None';
+  constructor( private formBuilder: FormBuilder, public dialog: MatDialog)
+  {
+    this.nuevaCuenta = this.formBuilder.
+    group({
+      Nombres: ['', Validators.required],
+      Apellidos: ['', Validators.required],
+      Run: ['', Validators.required],
+      Carrera: ['', Validators.required],
+      CorreInstitucional: ['', Validators.required],
+      CorreoPersonal: ['', Validators.required],
+      Contrasenna1: ['', Validators.required],
+      Contrasenna2: ['', Validators.required],
+      Rol: ['', Validators.required],
+    });
+  }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
+  }
+  onChangeCarrera(event: any): void
+  {
+    this.carreraActual = event;
+  }
+  onChangeRol(event: any): void
+  {
+    this.rolActual = event;
+  }
+  private openDialog(): void
+  {
+    this.dialog.open(DialogElementsExampleDialogComponent);
+  }
+  crear(): void
+  {
+    console.log(this.rolActual + ' ' + this.carreraActual);
+    if ( this.nuevaCuenta.value.Contrasenna1 === this.nuevaCuenta.value.Contrasenna2){
+      const nuevoUsuario: EncargadoCarrera = {
+        nombres : this.nuevaCuenta.value.Nombres,
+        apellidos : this.nuevaCuenta.value.Apellidos,
+        run : this.nuevaCuenta.value.Run,
+        carrera : this.nuevaCuenta.value.Carrera,
+        correInstitucional : this.nuevaCuenta.value.CorreInstitucional,
+        correoPersonal : this.nuevaCuenta.value.CorreoPersonal
+      };
+      console.log(nuevoUsuario);
+    }
+    else
+    {
+      this.openDialog();
+    }
   }
 
 }
--- GestionPracticas/src/app/components/SuperAdministrador/superadministrador.module.ts
+++ GestionPracticas/src/app/components/SuperAdministrador/superadministrador.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { MaterialModule } from "src/app/material/material.module";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from 'src/app/material/material.module';
 import { VerCuentasComponent } from './ver-cuentas/ver-cuentas.component';
 import { CrearCuentasSaComponent } from './crear-cuentas-sa/crear-cuentas-sa.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import { DialogElementsExampleDialogComponent } from './dialog/dialog-elements-example-dialog/dialog-elements-example-dialog.component';
 
 
 @NgModule({
     declarations: [
         // Aca van los modulos que se crean
-    VerCuentasComponent,
-        CrearCuentasSaComponent],
+      VerCuentasComponent,
+      CrearCuentasSaComponent,
+      DialogElementsExampleDialogComponent,
+    ],
     imports: [
         CommonModule,
         MaterialModule,
+        ReactiveFormsModule,
     ],
     exports: [
         // Aca van los modulos que se crean
