14e1633a5aea6ccc365645437185a746047368ab
Vicente Calderon <thunderwearsvicho37@protonmail.com>
2021-05-17 09:25:52 -0400

    Technically ready

--- GestionPracticas/src/app/components/AdministradorGeneral/crear-cuenta-estudiante/crear-cuenta-estudiante.component.ts
+++ GestionPracticas/src/app/components/AdministradorGeneral/crear-cuenta-estudiante/crear-cuenta-estudiante.component.ts
@@ -27,7 +27,7 @@ export class CrearCuentaEstudianteComponent implements OnInit {
 			Comuna: new FormControl(''),
 			Direccion: new FormControl(''),
 			Nacionalidad: new FormControl(''),
-		}
+		});
 	}
 	ngOnInit(): void {
 		throw new Error('Method not implemented.');
