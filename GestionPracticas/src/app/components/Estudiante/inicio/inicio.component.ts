45856a7c2d3c25a19428701320ee448dd2ebb577
j-ibanez-lopez <juanitodelasblancasnieves@gmail.com>
2021-05-24 22:49:47 -0400

    inicio v2
    algunos cambios de diseño

--- GestionPracticas/src/app/components/Estudiante/inicio/inicio.component.ts
+++ GestionPracticas/src/app/components/Estudiante/inicio/inicio.component.ts
@@ -1,6 +1,7 @@
 import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatCalendarCellCssClasses } from '@angular/material/datepicker/calendar-body';
 
 @Component({
 	selector: 'app-inicio',
@@ -23,6 +24,23 @@ export class InicioComponent implements OnInit {
 	// 	this.secondFormGroup = this._formBuilder.group({
 	// 		secondCtrl: ['', Validators.required]
 	// 	});
+	// }
+
+	fecha_seleccionada = new Date('2021/05/29')
+	fecha_seleccionada_arreglo = [new Date('2021/05/29'), new Date('2022/01/29')];
+	fechas_multiple: Boolean= true;
+	dateClass() {
+		return (fecha_seleccionada: Date): MatCalendarCellCssClasses => {
+			if (fecha_seleccionada.getDate() === 1) 
+			{return 'special-date';}
+			else 
+			{return 'nothing'}
+		};
+	}
+
+	// getFecha()
+	// {
+	// 	return this.fecha_seleccionada.getDate;
 	// }
 
 	ngOnInit(): void {
