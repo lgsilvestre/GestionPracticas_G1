8c44af8aeb43447916aba3445de7b1ed8c718f4e
matias ignacio erenchun marquez <matiaserenchun@gmail.com>
2021-06-05 22:08:40 -0400

    dialogo de error agregado al login
    se agrego un dialogo de erro que salta cuando alguno de los campos es incorrecto  o no se encontró, ademas de iniciarse la implementan de los diálogos estandarizado .

--- GestionPracticas/src/app/components/dialogs/error/error.component.ts
+++ GestionPracticas/src/app/components/dialogs/error/error.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {DialogDataModel} from '../../../model/dialogData.model';
 
 @Component({
   selector: 'app-error',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ErrorComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(MAT_DIALOG_DATA) public data: DialogDataModel) { }
 
   ngOnInit(): void {
   }
