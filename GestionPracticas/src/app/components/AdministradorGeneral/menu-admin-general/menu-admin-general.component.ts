f37ba46d6fa1776bbd947eac2bb89dd75f29f88c
kenox <eperedo13@alumnos.utalca.cl>
2021-06-30 16:37:13 -0400

    Corregido (Faltaba  mostrar el nombre en el menú inicial del admin)
    se muestra el nombre en el menú inicial del admin

--- GestionPracticas/src/app/components/AdministradorGeneral/menu-admin-general/menu-admin-general.component.ts
+++ GestionPracticas/src/app/components/AdministradorGeneral/menu-admin-general/menu-admin-general.component.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { AutenticacionService } from '../../Servicios/autenticacion.service';
 
 @Component({
 	selector: 'app-menu-admin-general',
 	templateUrl: './menu-admin-general.component.html',
 	styleUrls: ['./menu-admin-general.component.css', '../../../app.component.css']
 })
-export class MenuAdminGeneralComponent implements OnInit {
+export class MenuAdminGeneralComponent implements OnInit 
+{
+    adminGeneral: JSON;
 
-	constructor() {
+	constructor(private autenticacionService: AutenticacionService) 
+    {
+        this.adminGeneral = JSON.parse(localStorage.getItem('user')+'');
 	}
 
-	ngOnInit(): void {
+	ngOnInit(): void 
+    {
+
 	}
 
 }
