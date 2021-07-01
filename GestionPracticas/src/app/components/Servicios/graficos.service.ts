3fd3499eba35edf8c1bbeaf4985e554b5e56e61d
felipeOL <felipe.olave.r@hotmail.com>
2021-07-01 01:43:32 -0400

    update graficos.service.ts
    se agrego funcionalidad al servicio de graficos.

--- GestionPracticas/src/app/components/Servicios/graficos.service.ts
+++ GestionPracticas/src/app/components/Servicios/graficos.service.ts
@@ -1,9 +1,55 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GraficosService {
 
-  constructor() { }
+  constructor(private afStore: AngularFirestore) { }
+
+  //Metodo que obtiene el total de practicas aprobadas en general
+  obtenerInformacionPracticasAprobadas(){
+    return this.afStore.collection('SolicitudesPracticas', referencia => referencia
+    .where('estado','==','Aprobado'));
+  }
+
+  //Metodo que obtiene el total de practicas reprobadas en general
+  obtenerInformacionPracticasReprobadas(){
+    return this.afStore.collection('SolicitudesPracticas', referencia => referencia
+    .where('estado','==','Reprobado'));
+
+  }
+
+  //Metodo que obtiene el total de practicas pendientes en general
+  obtenerInformacionPracticasPendientes(){
+    return this.afStore.collection('SolicitudesPracticas', referencia => referencia
+    .where('estado','==','Pendiente'));
+  }
+
+  //Metodo que obtiene el total de practicas aprobadas de una carrera en especifico
+  obtenerInformacionPracticasAprobadasCarrera(carrera:string){
+    return this.afStore.collection('SolicitudesPracticas', referencia => referencia
+    .where('carrera','==',carrera)
+    .where('estado','==','Aprobado')
+    );
+  }
+
+  //Metodo que obtiene el total de practicas reprobadas de una carrera en especifico
+  obtenerInformacionPracticasReprobadasCarrera(carrera:string){
+    return this.afStore.collection('SolicitudesPracticas', referencia => referencia
+    .where('carrera','==',carrera)
+    .where('estado','==','Reprobado')
+    );
+  }
+
+  //Metodo que obtiene el total de practicas pendientes de una carrera en especifico
+  obtenerInformacionPracticasPendientesCarrera(carrera:string){
+    return this.afStore.collection('SolicitudesPracticas', referencia => referencia
+    .where('carrera','==',carrera)
+    .where('estado','==','Pendiente')
+    );
+  }
+
+
 }
