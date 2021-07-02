4b81f335a91988d141350b6b0ed52702af67bafd
kenox <eperedo13@alumnos.utalca.cl>
2021-07-01 22:20:32 -0400

    admin problem was solved

--- GestionPracticas/src/app/components/AdministradorGeneral/visualizar-practicas-admin/visualizar-practicas-admin.component.ts
+++ GestionPracticas/src/app/components/AdministradorGeneral/visualizar-practicas-admin/visualizar-practicas-admin.component.ts
@@ -53,12 +53,8 @@ export class VisualizarPracticasAdminComponent implements OnInit, AfterViewInit 
     displayedColumnsSolicitud: string[];
     displayedColumnsInscripcion: string[];
     displayedColumnsEnCurso: string[];
-    //Solicitud: matrícula, nombre, apellidos, rut, situación, accion
-    //Inscripcion: 'matricula', 'nombre', 'apellido', 'rut', 'empresa', 'situacion', 'accion'
-    //En curso: 'matricula', 'nombre', 'apellido', 'rut', 'empresa', 'situacion', 'accion'
-
     
-    solicitudes: Practica[];
+    solicitudes: any;
 
     //arreglo con los indices de las columnas que serán ignoradas al exportar al excel 
     //El valor 5 corresponde al indice de la columna acciones en la tabla solicitudes, la cual no debería mostrarse al exportar al excel
@@ -258,8 +254,8 @@ export class VisualizarPracticasAdminComponent implements OnInit, AfterViewInit 
             coleccion = 'Solicitudes'
         }
 
-        var solicitudRef = this.EC_service.update_solicitud(solicitud.idSolicitud, coleccion);
-        console.log("El id para actualizar es>>> " + solicitud.idSolicitud);
+        var solicitudRef = this.EC_service.update_solicitud(solicitud.id, coleccion);
+        console.log("El id para actualizar es>>> " + solicitud.id);
 
         if (param_estado == "Aceptado") {
             msg_success += "aceptada";
