09e0d932ae925660e0c6e61bfddd10bb45c019bf
felipeOL <felipe.olave.r@hotmail.com>
2021-07-05 21:29:50 -0400

    creacion de planes
    se cambio la modificacion de planes

--- GestionPracticas/src/app/components/AdministradorGeneral/crear-carrera/crear-carrera.component.ts
+++ GestionPracticas/src/app/components/AdministradorGeneral/crear-carrera/crear-carrera.component.ts
@@ -5,7 +5,6 @@ import { Carrera } from '../../../model/carreras.model';
 import { Router } from '@angular/router';
 import { PlanEstudios } from '../../../model/planEstudios.model';
 
-
 @Component({
   selector: 'app-crear-carrera',
   templateUrl: './crear-carrera.component.html',
@@ -16,6 +15,8 @@ export class CrearCarreraComponent implements OnInit {
   formularioCarrera:FormGroup;
   isLinear = false;
   formularioPlan:FormGroup;
+  plan:PlanEstudios[] = [];
+  opcionSelecionada:string = '';
 
   constructor(private _formBuilder: FormBuilder, private _gestionCarrera: GestionCarreraService, private route: Router)
   {
@@ -57,6 +58,17 @@ export class CrearCarreraComponent implements OnInit {
   volver()
   {
       this.route.navigate(['/gestionar-carreras']);
+  }
+
+  agregarPlan(){
+    const planEnCreacion:PlanEstudios = {
+      nombre: this.formularioPlan.value.nombrePlan,
+      numeroPracticas: this.opcionSelecionada,
+      requisitos: this.formularioPlan.value.requisitos,
+    }
+    console.log('Plan creado exitosamente:' + planEnCreacion.numeroPracticas);
+    this.plan.push(planEnCreacion);
+    console.log(this.plan.length);
   }
 
 }
--- GestionPracticas/src/app/model/planEstudios.model.ts
+++ GestionPracticas/src/app/model/planEstudios.model.ts
@@ -3,7 +3,7 @@ import { practicasCarrera } from "./practicasCarrera.model";
 export interface PlanEstudios
 {
     id?:string;
-    nombre: string;
-    requisitos:string[];
-    numeroPracticas:string;
+    nombre?: string;
+    requisitos?:string;
+    numeroPracticas?:string;
 }
