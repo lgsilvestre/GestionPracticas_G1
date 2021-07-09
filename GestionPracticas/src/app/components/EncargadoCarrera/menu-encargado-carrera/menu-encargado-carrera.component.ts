4e7233964cfd465cf9ef1bf8b90a1298372ce2d7
felipeOL <felipe.olave.r@hotmail.com>
2021-07-08 21:04:13 -0400

    graficos del encargado de carrera
    graficos del encargado de carrera.

--- GestionPracticas/src/app/components/EncargadoCarrera/menu-encargado-carrera/menu-encargado-carrera.component.ts
+++ GestionPracticas/src/app/components/EncargadoCarrera/menu-encargado-carrera/menu-encargado-carrera.component.ts
@@ -1,24 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { GraficosService } from '../../Servicios/graficos.service';
 
 @Component({
-	selector: 'app-menu-encargado-carrera',
-	templateUrl: './menu-encargado-carrera.component.html',
-	styleUrls: ['./menu-encargado-carrera.component.css', '../../../app.component.css']
+  selector: 'app-menu-encargado-carrera',
+  templateUrl: './menu-encargado-carrera.component.html',
+  styleUrls: ['./menu-encargado-carrera.component.css', '../../../app.component.css']
 })
-export class MenuEncargadoCarreraComponent implements OnInit 
-{
-    encargadoCarrera: any;
+export class MenuEncargadoCarreraComponent implements OnInit {
+  encargadoCarrera: any;
+  cantidadSolicitudesPracticasPendientes: number = 0;
+  user: any = JSON.parse(localStorage.getItem('user') || '{}');
 
-	constructor() 
-    {
-        this.encargadoCarrera = {nombres: '', apellidos: ''};
-        let encargado = JSON.parse(localStorage.getItem('user')+'');
-        this.encargadoCarrera.nombres   = encargado.nombres;
-        this.encargadoCarrera.apellidos = encargado.apellidos;
-    }
+  constructor(private _gestionGraficos: GraficosService) {
+    this.encargadoCarrera = { nombres: '', apellidos: '' };
+    let encargado = JSON.parse(localStorage.getItem('user') + '');
+    this.encargadoCarrera.nombres = encargado.nombres;
+    this.encargadoCarrera.apellidos = encargado.apellidos;
 
-	ngOnInit(): void 
-    {
-    }
+    this._gestionGraficos.obtenerInformacionSolicitudPracticasPendientesCarrera(this.user.carrera).valueChanges().subscribe(datos => {
+      this.cantidadSolicitudesPracticasPendientes = datos.length;
+    })
+  }
+
+  ngOnInit(): void {
+  }
 
 }
--- GestionPracticas/src/app/components/Graficos/graficos.module.ts
+++ GestionPracticas/src/app/components/Graficos/graficos.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from "@angular/core";
 import { ChartsModule } from 'ng2-charts';
 import { BarChartComponent } from "./bar-chart/bar-chart.component";
 import { PieChartComponent } from "./pie-chart/pie-chart.component";
+import { GraficoCircularEncCarreraComponent } from './grafico-circular-enc-carrera/grafico-circular-enc-carrera.component';
+import { GraficoSolicitudEncCarreraComponent } from './grafico-solicitud-enc-carrera/grafico-solicitud-enc-carrera.component';
 
 
 @NgModule({
   declarations: [
     BarChartComponent,
     PieChartComponent,
+    GraficoCircularEncCarreraComponent,
+    GraficoSolicitudEncCarreraComponent,
   ],
   imports: [
     CommonModule,
@@ -17,6 +21,8 @@ import { PieChartComponent } from "./pie-chart/pie-chart.component";
   exports: [
     BarChartComponent,
     PieChartComponent,
+    GraficoCircularEncCarreraComponent,
+    GraficoSolicitudEncCarreraComponent,
   ]
 })
 export class GraficosModule{}
